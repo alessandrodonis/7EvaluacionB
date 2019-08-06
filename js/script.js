0753fe0fd3c50e1140543c5534cff1d464fd895f
alessandrodonis <alessandrodonis17@gmail.com>
2019-08-06 08:32:46 -0600

    ahora si xd

---
+++ js/script.js
@@ -0,0 +1,12 @@
+warcraft.controller("world", fuction($rootScope,$scope,$http){
+	$scope.craft = [] ;
+	{
+    "_links": {
+        "self": {
+            "href": "https://us.api.blizzard.com/data/wow/token/?namespace=dynamic-us"
+        }
+    },
+    "last_updated_timestamp": 1536597360000,
+    "price": 1138480000
+
+}
